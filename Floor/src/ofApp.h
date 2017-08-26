4378025feece23a19b64a9767b48af154ac29720
Atsushi Tadokoro <tadokoro@gmail.com>
2017-08-27 08:23:41 +0900

    add Floor App

---
+++ Floor/src/ofApp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxTuio.h"
+
+class ofApp : public ofSimpleApp {
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void objectAdded(ofxTuioObject & tuioObject);
+	void objectRemoved(ofxTuioObject & tuioObject);
+	void objectUpdated(ofxTuioObject & tuioObject);
+
+	void tuioAdded(ofxTuioCursor & tuioCursor);
+	void tuioRemoved(ofxTuioCursor & tuioCursor);
+	void tuioUpdated(ofxTuioCursor & tuioCursor);
+
+	ofxTuioClient tuio;
+	string log;
+    ofxOscSender sender;
+};
